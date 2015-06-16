934839609c7641139444e51a556eb0022b244a6f
Gerwin Brunner <gerwin.brunner@gmail.com>
2015-06-16 15:29:28 +0200

    added blaze dependency

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'vilango:detect-app-environment',
   summary: 'Detect and expose the app environment we are running in right now',
-  version: '1.0.4',
+  version: '1.0.5',
   git: 'https://github.com/Vilango/detect-app-environment',
   documentation: null
 });
@@ -9,7 +9,7 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
   api.use(['coffeescript'], 'client');
-  api.use('deps', 'client');
+  api.use(['deps','blaze'], 'client');
   //api.use('standard-app-packages');
   api.addFiles('detect-app-environment.coffee', 'client');
   api.addFiles('detect-app-helper.coffee', 'client');
